a681d921d75d09bfdce5eca3d0bd3b18842b3947
hey-aubs <email.aubs@gmail.com>
2022-03-04 21:52:41 -0800

    synces

---
+++ HW/Homework 06/aubrey_solution/js/trulia.js
@@ -0,0 +1,33 @@
+let truliaCards = document.getElementsByClassName("trulia-grid-item");
+
+document.addEventListener('DOMContentLoaded', function(event) {
+
+// Toggle the navigation
+document.querySelector(".trulia-nav-toggle").addEventListener('click',function(e){
+  e.preventDefault();
+  let navSet = document.querySelectorAll('.trulia-nav > nav ul');
+
+  for(let n=0;n<navSet.length;n++){
+      navSet[n].classList.toggle('trulia-nav-mobilehide');
+  }
+});
+
+// Loop through all the cards
+for (var i = 0; 
+  i < truliaCards.length; i++){
+    
+  // Add a click listener on each card
+  truliaCards[i].addEventListener('click',function(){
+      
+      // Remove the featured class
+      for (var q = 0; 
+        q < truliaCards.length; q++){
+          truliaCards[q].classList.remove('trulia-featured-grid-item');
+      } 
+
+      // Add the featured class on the one clicked on
+      this.classList.add('trulia-featured-grid-item');
+  });
+}
+
+  });  
